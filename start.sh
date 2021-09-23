42e8fb42ca4bc38509352bab31533e1e56abfdc3
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-23 10:14:37 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/ZauteKm/URL-Shortner-Bot.git /URL-Shortner-Bot
+cd /URL-Shortner-Bot
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
